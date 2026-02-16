3ad9aa148ec4d70970ba9c6b512d2b29d6095ec3
SergeyChernov1995 <79245989+SergeyChernov1995@users.noreply.github.com>
2026-02-16 15:24:20 +0500

    Update 1vs100.py

--- 1vs100.py
+++ 1vs100.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 # ----------------------------------------------------------------------------
 # 1 vs. 100
-# Copyright © 2021 Sergey Chernov aka Gamer
+# Copyright © 2021-2026 Sergey Chernov aka Gamer
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
